dfa0ed3bbdfb48bdfa05a3ef7dccd9a7dcca27cd
YaroslavRogo <rogozinskijarik@gmail.com>
2024-04-11 18:42:09 +0400

    RYR_1

--- Lesson 4/Task 6.py
+++ Lesson 4/Task 6.py
@@ -2,98 +2,76 @@ import tkinter as tk
 from PIL import ImageTk, Image
 from pathlib import Path
 
-# Создаем окно root для tkinter
 root = tk.Tk()
 root.title("Photo Viewer")
 
-# Создаем объекты-фотографии
 photo_images = []
+current_index = 0
 
-# Создаем функцию, которая будет вызываться после создания корневого окна
 def create_photo_images():
     global photo_images
 
-    # Создаем список путей к изображениям
-    image_folder = Path("images")  # Папка с изображениями
+    image_folder = Path("images")
     image_paths = list(image_folder.glob("*.jpg"))
 
-    # Проверяем наличие изображений
-    if not image_paths:
-        print("В папке images нет изображений.")
-        root.quit()
-        return
-
-    # Создаем объекты-фотографии
     for path in image_paths:
-        try:
-            image = Image.open(path)
-            photo = ImageTk.PhotoImage(image)
-            photo_images.append(photo)
-        except Exception as e:
-            print(f"Ошибка при открытии файла {path}: {str(e)}")
-            continue
+        image = Image.open(path)
+        photo = ImageTk.PhotoImage(image)
+        photo_images.append(photo)
 
-    # Проверяем наличие фотографий
-    if not photo_images:
-        print("Не удалось создать объекты-фотографии.")
-        root.quit()
+    status_label.config(text=f"Фотография 1/{len(photo_images)}")
+    show_image(0)
+
+def show_image(idx):
+    global current_image_label, photo_images, current_index
+
+    idx = max(0, min(idx, len(photo_images) - 1))
+    if idx == current_index:
         return
 
-# Функция для отображения текущей фотографии
-def show_image(idx):
-    global current_image_label, photo_images
+    current_index = idx
 
-    # Удостоверяемся, что индекс находится в допустимом диапазоне
-    idx = max(0, min(idx, len(photo_images) - 1))
-
-    # Обновляем отображение фотографии
     current_image_label.config(image=photo_images[idx])
 
-    # Обновляем информацию о текущей фотографии и их общем количестве
-    status_label.config(text=f"Фотография {idx + 1}/{len(photo_images)}")
+    status_label.config(text=f"Фотография {idx+1}/{len(photo_images)}")
 
-    # Обновляем состояния кнопок пролистывания
-    button_back.config(state=tk.NORMAL if idx > 0 else tk.DISABLED)
-    button_forward.config(state=tk.NORMAL if idx < len(photo_images) - 1 else tk.DISABLED)
+    button_backward.config(state=tk.DISABLED if current_index == 0 else tk.NORMAL)
+    button_forward.config(state=tk.DISABLED if current_index == len(photo_images) - 1 else tk.NORMAL)
 
-# Функция для пролистывания фотографий влево
-def back():
+def backward():
     global current_index
     show_image(current_index - 1)
-    current_index -= 1
 
-# Функция для пролистывания фотографий вправо
 def forward():
     global current_index
     show_image(current_index + 1)
-    current_index += 1
 
-# Функция для выхода из программы
 def exit_program():
     root.quit()
 
-# Создаем кнопки пролистывания фотографий
-current_index = 0
-button_back = tk.Button(root, text="<<", command=back, state=tk.DISABLED)
+button_backward = tk.Button(root, text="<<", command=backward, state=tk.DISABLED)
 button_forward = tk.Button(root, text=">>", command=forward)
 button_exit = tk.Button(root, text="Exit", command=exit_program)
 
-# Пакуем кнопки с использованием grid
-button_back.grid(row=1, column=0, padx=10, pady=10)
+button_backward.grid(row=1, column=0, padx=10, pady=10)
 button_forward.grid(row=1, column=2, padx=10, pady=10)
 button_exit.grid(row=1, column=1, padx=10, pady=10)
 
-# Создаем информационный виджет
 status_label = tk.Label(root, text="", bd=1, relief=tk.SUNKEN, anchor=tk.E)
 status_label.grid(row=2, column=0, columnspan=3, sticky=tk.W+tk.E, padx=10, pady=10)
 
-# Создаем первую фотографию (пока без изображения)
 current_image_label = tk.Label(root)
 current_image_label.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-# Вызываем функцию создания объектов-фотографий после создания окна root
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+
 root.bind("<Map>", lambda event: create_photo_images())
 
-# Передаем управление tkinter
 root.geometry("700x700")
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(2, weight=1)
+
 root.mainloop()
